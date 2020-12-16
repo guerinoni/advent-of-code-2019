e96320860d3821a5e2d63f257199f7fb6662baca
Federico Guerinoni <guerinoni.federico@gmail.com>
2020-12-16 16:36:05 +0100

    Refactor structure of project
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-#![allow(dead_code)]
-
 mod day1;
 mod day2;
 mod day3;
+mod solver;
+
+use solver::*;
 
 fn main() {
     println!("Advent of code 2019!");
+
+    let d = day1::Day1::new("input/day1");
+    println!("Day1 -> {}", d.solve());
+
+    let d = day2::Day2::new("input/day2");
+    println!("Day2 -> {}", d.solve());
+
+    let d = day3::Day3::new("input/day3");
+    println!("Day3 -> {}", d.solve());
 }
