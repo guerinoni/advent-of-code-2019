b29056af7bae8b9b11ee6a91abe7b60edae62f91
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-01-02 18:23:22 +0100

    Solve part2
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/day4.rs
+++ src/day4.rs
@@ -41,7 +41,7 @@ impl Solver for Day4 {
         format!(
             "Solution part1 -> {}\n\tSolution part2 -> {}",
             self.part1(f[0], f[1]),
-            0
+            self.part2(f[0], f[1])
         )
     }
 }
@@ -91,6 +91,45 @@ fn digit_increase(number: i64) -> bool {
     true
 }
 
+/*
+ * --- Part Two ---
+ * An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+ *
+ * Given this additional criterion, but still ignoring the range rule,
+ * the following are now true:
+ *
+ * 112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+ * 123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+ * 111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+ * How many different passwords within the range given in your puzzle input meet all of the criteria?
+ */
+
+impl Day4 {
+    fn part2(&self, from: i64, to: i64) -> i64 {
+        let mut counter = 0;
+        for i in from..to {
+            if has_adjacent_same_digit(i) && digit_increase(i) {
+                counter = counter + 1;
+            }
+        }
+
+        counter
+    }
+}
+
+fn has_adjacent_same_digit(number: i64) -> bool {
+	let x: Vec<_> = format!("{}", number).chars().collect();
+	let mut same = HashMap::<char, usize>::new();
+	for w in x.windows(2) {
+		let a = w[0];
+		let b = w[1];
+		if a == b {
+			*same.entry(a).or_default() += 1;
+		}
+	}
+	same.iter().filter(|x| *x.1 == 1).count() > 0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -99,6 +138,10 @@ mod tests {
     fn validation() {
         assert_eq!(has_digit_same(122345), true);
         assert_eq!(has_digit_same(123456), false);
+        assert_eq!(has_adjacent_same_digit(112233), true);
+        assert_eq!(has_adjacent_same_digit(123444), false);
+        assert_eq!(has_adjacent_same_digit(111122), true);
+        assert_eq!(has_adjacent_same_digit(111241), false);
 
         assert_eq!(digit_increase(123456), true);
         assert_eq!(digit_increase(111111), true);
@@ -106,5 +149,6 @@ mod tests {
 
         let d = Day4::new("");
         assert_eq!(d.part1(165432, 707912), 1716);
+        assert_eq!(d.part2(165432, 707912), 1163);
     }
 }
